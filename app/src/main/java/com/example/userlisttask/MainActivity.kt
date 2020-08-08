83a5d58828fc8e80973c7cbd496933c96f68d7e9
patrik <patrik.gutek1@gmail.com>
2020-08-08 20:07:15 +0200

    Getting response from the site

--- app/src/main/java/com/example/userlisttask/MainActivity.kt
+++ app/src/main/java/com/example/userlisttask/MainActivity.kt
@@ -5,22 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import java.io.IOException
-import java.time.Duration
 
 class MainActivity : AppCompatActivity() {
+
+    val usersList : ArrayList<User> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         recyclerView_UserList.layoutManager = LinearLayoutManager(this)
-        recyclerView_UserList.adapter = UserListAdapter()
+        recyclerView_UserList.adapter = UserListAdapter(usersList)
 
         fetchData()
+
+
     }
 
     fun fetchData() {
@@ -33,24 +36,35 @@ class MainActivity : AppCompatActivity() {
         val request = Request.Builder().url(url).build()
 
         val client = OkHttpClient()
+        //make a thread to execute
         client.newCall(request).enqueue(object : Callback {
+            //create a toast to inform a user
             override fun onFailure(call: Call, e: IOException) {
                 runOnUiThread() {
                     Toast.makeText(
                         getContext(),
                         "Unable to fetch list of users",
                         Toast.LENGTH_SHORT
-                    )
-                        .show()
+                    ).show()
                 }
             }
 
+            //write down users to inform user its successful
             override fun onResponse(call: Call, response: Response) {
                 val text = response.body?.string()
+                val gson = GsonBuilder().create()
+                val users = gson.fromJson(text, UserList::class.java)
+                usersList.addAll(users.data)
+                runOnUiThread {
+                    recyclerView_UserList.adapter?.notifyDataSetChanged()
+                }
 
-                println(text)
             }
         })
 
     }
+
+    //names of vals are to be exactly the same as in json file (i.e. the same as in response)
+    class UserList(val data : List<User>)
+    class User(val id: Int, val first_name: String, val last_name: String)
 }
--- app/src/main/java/com/example/userlisttask/UserListAdapter.kt
+++ app/src/main/java/com/example/userlisttask/UserListAdapter.kt
@@ -4,25 +4,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.single_user_layout.view.*
+import kotlinx.android.synthetic.main.single_user_for_recycler_view_layout.view.*
 
-class UserListAdapter: RecyclerView.Adapter<SingleUserView>() {
-
-    val listOfNames = listOf("Peder", "Jozef", "Marek")
+class UserListAdapter(val users: List<MainActivity.User>): RecyclerView.Adapter<SingleUserView>() {
 
     override fun getItemCount(): Int {
-        return listOfNames.size
+        return users.count()
     }
 
+    //creates the row with default values
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SingleUserView {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val cellForRow = layoutInflater.inflate(R.layout.single_user_layout, parent, false)
+        val cellForRow = layoutInflater.inflate(R.layout.single_user_for_recycler_view_layout, parent, false)
 
         return SingleUserView(cellForRow)
     }
 
+    //sets the custom data to the row
     override fun onBindViewHolder(holder: SingleUserView, position: Int) {
-        holder.view.userName.text = listOfNames.get(position)
+
+        holder.view.setOnClickListener(View.OnClickListener {
+
+        })
+        holder.view.userName.text = users.get(position).first_name + " " + users.get(position).last_name
     }
 }
 
